d0123c8e5098564351a83921cbb3631aa36611c7
Duy <nova411992@gmail.com>
2020-08-11 15:23:59 +0700

    Update autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -1,21 +1,29 @@
 $(function () {
     var availableTags = [
-        "one day is the first and last today", "tuesday is today", "one", "one day is tomorrow"
+        {label: "Hilton Honors Card",value: "Hilton Honors Card"},
+        {label: "Hilton Honors Amex Aspire Card", value: "Hilton Honors Amex Aspire Card"},
+        {label: "Hilton Honors Amex Business Card", value: "Hilton Honors Amex Business Card"},
+        {label: "Amex Hilton Surpass Card", value: "Amex Hilton Surpass Card"},
+        {label: "U.S. Bank FlexPerks Gold Amex Card", value: "U.S. Bank FlexPerks Gold Amex Card"},
+        {label: "Amex Gold Business", value: "Amex Gold Business"},
+        {label: "Amex Gold Delta Business", value: "Amex Gold Delta Business"},
+        
     ];
 
     // Overrides the default autocomplete filter function to search for matched on atleast 1 word in each of the input term's words
     $.ui.autocomplete.filter = function (array, terms) {
         arrayOfTerms = terms.split(" ");
-        var term = $.map(arrayOfTerms, function (tm) {
-             return $.ui.autocomplete.escapeRegex(tm);
-        }).join('|');
-       var matcher = new RegExp("\\b" + term, "i");
-        return $.grep(array, function (value) {
-           return matcher.test(value.label || value.value || value);
+        arrayOfTerms.forEach(function (entry) {
+        array = $.grep(array, function (value) {
+             //   console.log(value);
+                return value.label.toLowerCase().indexOf(entry.toLowerCase()) >= 0 || value.value.toLowerCase().indexOf(entry.toLowerCase()) >= 0;
+            })
         });
+
+       return array;
     };
 
-    $("#tags").autocomplete({
+    $("#input").autocomplete({
         source: availableTags,
         multiple: true,
         mustMatch: false
